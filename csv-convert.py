d1e1d5cf2f343b26b0eb091e1b938ada22d7ca30
Fabian Zimmermann <dev.faz@gmail.com>
2021-04-29 08:46:00 +0200

    added date-filter based to csv-convert
    by default dkb filters based on billing date, this may cause missed entries in ff3

--- csv-convert.py
+++ csv-convert.py
@@ -4,12 +4,15 @@ import csv
 import pylightxl as xl
 import pandas as pd
 import re
+import datetime
 
 parser = argparse.ArgumentParser(description='convert csv')
 parser.add_argument('--input', dest='input', help='file to read csv', required=True)
 parser.add_argument('--output', dest='output', help='file to read csv', default='output.csv', required=False)
 parser.add_argument('--delimiter', dest='delimiter', help='str to delimit', default=';')
 parser.add_argument('--write-delimiter', dest='wdelimiter', help='str to delimit', default=',')
+parser.add_argument('--date-filter-field', dest='dffield', type=int, help='field to apply date-filter on', default='1')
+parser.add_argument('--date', dest='date', help='date to apply on date-filter-field', required=False)
 
 args = parser.parse_args()
 
@@ -29,9 +32,18 @@ for row in reader:
   if len(row) > 0 and re.match("(Referenznummer|transactionId|Umsatz)", row[0]):
     found = 1
 
+  if found != 1:
+    continue
+
   length = len(row)
   for i in range(length):
     row[i] = row[i].replace("€", "").strip()
 
-  if found == 1:
-    writer.writerow(row)
+  if args.date:
+    if re.match('\d{2}.\d{2}.\d{4}', row[args.dffield]):
+        line_date = datetime.datetime.strptime(row[args.dffield], '%d.%m.%Y')
+        filter_date = datetime.datetime.strptime(args.date, '%d.%m.%Y')
+        if line_date < filter_date:
+          continue
+
+  writer.writerow(row)
--- start-kk.sh
+++ start-kk.sh
@@ -4,33 +4,35 @@ set -e
 WORKPATH="$HOME/.aqbanking"
 TODATE_PATH="$WORKPATH/TODATE_KK"
 
+FETCH_FROMDATE=$( date +'%d.%m.%Y' -d '30 day ago' )
+
 source $WORKPATH/env
 
 if [ -s ${TODATE_PATH} ]
 then
-  FROMDATE=$( date +'%d.%m.%Y' -d "$( < ${TODATE_PATH} ) + 1 day ")
-  if [ "${FROMDATE}" = "$( date +'%d.%m.%Y' )" ]
-  then
-    echo "Nothing to do!"
-    exit 0
-  fi
+  FROMDATE=$( date +'%d.%m.%Y' -d "$( < ${TODATE_PATH} )" )
 else
   FROMDATE=$( date +'%d.%m.%Y' -d '2 day ago' )
 fi
+
 if [ -z "$TODATE" ]
 then
   TODATE="$( date +'%d.%m.%Y' -d '1 day ago' )"
   TODATE_SAFE="$( date +%F -d '1 day ago')"
 fi
 
+if [ "$FROMDATE" = "$TODATE" ]
+then
+  echo "Nothing to do"
+  exit 0
+fi
 
 TMP=$( mktemp -d )
 
 export PIN=$( grep PIN_ ~/.aqbanking/pinfile | cut -d= -f 2 | xargs )
 
-python dkb-visa/dkb.py --userid $DKB_USER --cardid $DKB_CARDID --output=${TMP}/kk.latin1.csv --raw --from-date="${FROMDATE}" --to-date="${TODATE}" 
-iconv -f latin1 -t utf-8 ${TMP}/kk.latin1.csv > ${TMP}/kk.utf8.csv
-./csv-convert.py --input ${TMP}/kk.utf8.csv --output output.kk.csv
+python dkb-visa/dkb.py --userid $DKB_USER --cardid $DKB_CARDID --output=${TMP}/kk.csv --raw --from-date="${FETCH_FROMDATE}" --to-date="${TODATE}"
+./csv-convert.py --input ${TMP}/kk.csv --output output.kk.csv --date ${FROMDATE}
 echo rm -v ${FILE}
 
 echo ${TODATE_SAFE} > ${TODATE_PATH}
