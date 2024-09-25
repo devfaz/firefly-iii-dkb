f49cf8aff6cb6e327d14eaf0e0ed7e16c822306f
Fabian Zimmermann <dev.faz@gmail.com>
2024-09-25 09:38:49 +0200

    autoimport: return failure if data-import fails

--- autoimport.sh
+++ autoimport.sh
@@ -4,7 +4,7 @@ set -e -o pipefail
 source $HOME/.aqbanking/env
 KTO=$1
 
-curl --location --request POST "${AUTOIMPORT_URL}" \
+curl --fail-with-body --location --request POST "${AUTOIMPORT_URL}" \
 --header 'Accept: application/json' \
 --form 'importable=@"./'${KTO}'.csv"' \
 --form 'json=@"./import_config_'${KTO}'.json"'
