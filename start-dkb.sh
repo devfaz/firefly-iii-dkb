169043a921eb9338df5a85871928b08b9f0084b3
Fabian Zimmermann <dev.faz@gmail.com>
2025-09-30 11:05:50 +0200

    feat: keep window open after run

--- start-dkb.sh
+++ start-dkb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -e -o pipefail
+set -e -o pipefail -u
 
 IMAGE=${1:-ghcr.io/devfaz/firefly-iii-dkb:latest}
-exec podman run --pull newer --rm -it --userns=keep-id -v $HOME/.aqbanking/:/home/aqbanking/.aqbanking/ "${IMAGE}"
+podman run --pull newer --rm -it --userns=keep-id -v $HOME/.aqbanking/:/home/aqbanking/.aqbanking/ "${IMAGE}"
+
+read -p "Hit ENTER to continue"
