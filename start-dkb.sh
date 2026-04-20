2bf26c1896af7a77af5dfd465686a979490dc89d
Fabian Zimmermann <dev.faz@gmail.com>
2026-04-20 12:20:11 +0200

    feat: keep terminal open if something went wrong

--- start-dkb.sh
+++ start-dkb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -o pipefail -u
 
+trap "read -p 'Hit ENTER to continue'" EXIT
+
 IMAGE=${1:-ghcr.io/devfaz/firefly-iii-dkb:latest}
 podman run --pull newer --rm -it --userns=keep-id -v $HOME/.aqbanking/:/home/aqbanking/.aqbanking/ "${IMAGE}"
-
-read -p "Hit ENTER to continue"
