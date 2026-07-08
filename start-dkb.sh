4f9f977108a1be30fe20858b69e944d9aad12e7e
Fabian Zimmermann <dev.faz@gmail.com>
2026-07-08 18:50:45 +0200

    feat: start-dkb now detects if podman or docker is used

--- start-dkb.sh
+++ start-dkb.sh
@@ -4,4 +4,11 @@ set -e -o pipefail -u
 trap "read -p 'Hit ENTER to continue'" EXIT
 
 IMAGE=${1:-ghcr.io/devfaz/firefly-iii-dkb:latest}
-podman run --pull newer --rm -it --userns=keep-id -v $HOME/.aqbanking/:/home/aqbanking/.aqbanking/ "${IMAGE}"
+
+if which podman 1>/dev/null; then
+	CMD="podman run --pull newer --rm -it --userns=keep-idi"
+else
+	CMD="docker run --pull always --rm -it -u $(id -u):$(id -g)"
+fi
+
+$CMD -v $HOME/.aqbanking/:/home/aqbanking/.aqbanking/ "${IMAGE}"
